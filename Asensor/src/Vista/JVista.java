efb82990d160dd343f2c1dd0610fb7be7d5a1c82
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-10-10 19:34:25 -0500

    Asensor Vista

--- Asensor/src/Vista/JVista.java
+++ Asensor/src/Vista/JVista.java
@@ -5,23 +5,207 @@ public class JVista extends javax.swing.JFrame {
 
     public JVista() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        btn_Agregar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbl_4Piso = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        btn_Plant = new javax.swing.JButton();
+        btn_Recep = new javax.swing.JButton();
+        btn_Sist = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tbl_3Piso = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tbl_Disponible = new javax.swing.JTable();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        tbl_Ingreso = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+        setResizable(false);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setLabelFor(tbl_4Piso);
+        jLabel1.setText("Personal Disponible");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(94, 15, -1, -1));
+
+        btn_Agregar.setText("+");
+        jPanel1.add(btn_Agregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(309, 11, -1, -1));
+
+        tbl_4Piso.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Momento", "Momento", "Orden"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tbl_4Piso.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(tbl_4Piso);
+        if (tbl_4Piso.getColumnModel().getColumnCount() > 0) {
+            tbl_4Piso.getColumnModel().getColumn(1).setResizable(false);
+            tbl_4Piso.getColumnModel().getColumn(2).setResizable(false);
+            tbl_4Piso.getColumnModel().getColumn(3).setResizable(false);
+        }
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 50, 340, 150));
+
+        jLabel2.setText("Personal Disponible");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(97, 254, -1, -1));
+
+        btn_Plant.setText("3 Planta");
+        jPanel1.add(btn_Plant, new org.netbeans.lib.awtextra.AbsoluteConstraints(119, 286, 84, 42));
+
+        btn_Recep.setText("1 Recepcion");
+        jPanel1.add(btn_Recep, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 286, -1, 42));
+
+        btn_Sist.setText("3 Sistemas");
+        jPanel1.add(btn_Sist, new org.netbeans.lib.awtextra.AbsoluteConstraints(221, 286, -1, 42));
+
+        jLabel3.setLabelFor(tbl_3Piso);
+        jLabel3.setText("Personal de Ingreso");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(103, 346, -1, -1));
+
+        tbl_3Piso.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Peso", "Momento", "Orden"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tbl_3Piso.getTableHeader().setReorderingAllowed(false);
+        jScrollPane2.setViewportView(tbl_3Piso);
+        if (tbl_3Piso.getColumnModel().getColumnCount() > 0) {
+            tbl_3Piso.getColumnModel().getColumn(1).setResizable(false);
+            tbl_3Piso.getColumnModel().getColumn(2).setResizable(false);
+            tbl_3Piso.getColumnModel().getColumn(3).setResizable(false);
+        }
+
+        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(834, 156, 360, 140));
+
+        jButton1.setText("Ingresar");
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 420, 80, 50));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/Sin título.png"))); // NOI18N
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 70, -1, -1));
+
+        tbl_Disponible.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Codigo", "Peso"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tbl_Disponible.getTableHeader().setReorderingAllowed(false);
+        jScrollPane3.setViewportView(tbl_Disponible);
+        if (tbl_Disponible.getColumnModel().getColumnCount() > 0) {
+            tbl_Disponible.getColumnModel().getColumn(1).setResizable(false);
+            tbl_Disponible.getColumnModel().getColumn(2).setResizable(false);
+        }
+
+        jPanel1.add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, 294, 196));
+
+        tbl_Ingreso.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Piso", "Momento"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tbl_Ingreso.getTableHeader().setReorderingAllowed(false);
+        jScrollPane4.setViewportView(tbl_Ingreso);
+        if (tbl_Ingreso.getColumnModel().getColumnCount() > 0) {
+            tbl_Ingreso.getColumnModel().getColumn(1).setResizable(false);
+            tbl_Ingreso.getColumnModel().getColumn(2).setResizable(false);
+        }
+
+        jPanel1.add(jScrollPane4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 378, 294, 196));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -35,7 +219,7 @@ public class JVista extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -60,5 +244,23 @@ public class JVista extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_Agregar;
+    private javax.swing.JButton btn_Plant;
+    private javax.swing.JButton btn_Recep;
+    private javax.swing.JButton btn_Sist;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTable tbl_3Piso;
+    private javax.swing.JTable tbl_4Piso;
+    private javax.swing.JTable tbl_Disponible;
+    private javax.swing.JTable tbl_Ingreso;
     // End of variables declaration//GEN-END:variables
 }
