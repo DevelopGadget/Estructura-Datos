18f857b2b91a740a02e374e8260ce27865ace9a5
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-11-03 15:18:33 -0500

    Nodo

--- Arboles/src/Modelos/Nodo.java
+++ Arboles/src/Modelos/Nodo.java
@@ -2,4 +2,38 @@ package Modelos;
 
 public class Nodo {
     
+    private String Valor;
+    private Nodo NodoIzqu;
+    private Nodo NodoDere;
+
+    public Nodo(String Valor, Nodo NodoIzqu, Nodo NodoDere) {
+        this.Valor = Valor;
+        this.NodoIzqu = NodoIzqu;
+        this.NodoDere = NodoDere;
+    }
+
+    public String getValor() {
+        return Valor;
+    }
+
+    public void setValor(String Valor) {
+        this.Valor = Valor;
+    }
+
+    public Nodo getNodoIzqu() {
+        return NodoIzqu;
+    }
+
+    public void setNodoIzqu(Nodo NodoIzqu) {
+        this.NodoIzqu = NodoIzqu;
+    }
+
+    public Nodo getNodoDere() {
+        return NodoDere;
+    }
+
+    public void setNodoDere(Nodo NodoDere) {
+        this.NodoDere = NodoDere;
+    }
+    
 }
